bd6287f1411418827f5906ae542010b06835209a
Highlight Clark <highlightz@163.com>
2015-08-18 16:56:49 +0800

    Update ControlStrategy.cpp

--- ControlStrategy.cpp
+++ ControlStrategy.cpp
@@ -5,12 +5,12 @@ dg.genWaypoint( distance, x_next, y_next );
 
 odometry anOdom = libviso2.getOdom( );
 
-// Calculate the angel to waypoint
+// Calculate the angle to waypoint
 const pi = 3.141592;
 double angleToWaypoint = 0.0;
 if ( anOdom.x == - y_next )
 {
-  angleToWaypoint = pi / 2;  // Or -pi/2
+  angleToWaypoint = pi / 2;  // Or -pi/2? How to tell this?
 }
 else
 {
@@ -18,9 +18,7 @@ else
   angleToWaypoint = atan( k );
 }
 
-double angleFromLaserToWaypoint = angleToWaypoint - ( 90 - anOdom.yaw );
+double angleFromLaserToWaypoint = angleToWaypoint * 180 / pi - ( 90 - anOdom.yaw * 180 / pi );
 
 const double weightOfLocal = 0.6;
 double controlAngle = weightOfLocal * angleOfSafety + ( 1 - weightOfLocal ) * angleFromLaserToWaypoint;
-
-
