fd45657a903ae74a6402b50cf8a633ced7ae6c80
Highlight Clark <highlightz@163.com>
2015-08-26 21:47:59 +0800

    Update sendControls.cpp

--- sendControls.cpp
+++ sendControls.cpp
@@ -18,14 +18,14 @@ send[1] = 0xfe;
 
 // Speed control
 const int *p = (int *)&spd;
-send[2] = (*p>>24)&0xff;
-send[3] = (*p>>16)&0xff;
-send[4] = (*p>>8)&0xff;
-send[5] = (*p>>0)&0xff;
+send[5] = (*p>>24)&0xff;
+send[4] = (*p>>16)&0xff;
+send[3] = (*p>>8)&0xff;
+send[2] = (*p>>0)&0xff;
 
 // Angle control
 p = (int *)&ang;
-send[6] = (*p>>24)&0xff;
-send[7] = (*p>>16)&0xff;
-send[8] = (*p>>8)&0xff;
-send[9] = (*p>>0)&0xff;
+send[9] = (*p>>24)&0xff;
+send[8] = (*p>>16)&0xff;
+send[7] = (*p>>8)&0xff;
+send[6] = (*p>>0)&0xff;
